0aeb48cc04a9c7b1841ffa3e23d6eb9377f025b8
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-01-05 22:20:39 -0500

    Implementar la lista de usuarios en la página de inicio con CoreUI y axios, y actualizar el idioma base del HTML y el título.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   CContainer,
   CTable,
@@ -8,37 +8,42 @@ import {
   CTableHeaderCell,
   CTableRow,
 } from "@coreui/react";
+import axios from "axios";
 
 const Home = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    cargarUsuarios();
+  }, []);
+
+  const cargarUsuarios = async () => {
+    const usuarios = await axios.get("http://localhost:8080/api/v1/usuarios");
+    console.log(usuarios.data);
+    setUsers(usuarios.data);
+  };
   return (
     <CContainer className="mt-3">
       <CTable className="border">
         <CTableHead>
           <CTableRow>
             <CTableHeaderCell scope="col">N°</CTableHeaderCell>
+            <CTableHeaderCell scope="col">Nombre de Usuario</CTableHeaderCell>
             <CTableHeaderCell scope="col">Nombre</CTableHeaderCell>
             <CTableHeaderCell scope="col">Apellido</CTableHeaderCell>
             <CTableHeaderCell scope="col">Email</CTableHeaderCell>
           </CTableRow>
         </CTableHead>
         <CTableBody>
-          <CTableRow>
-            <CTableHeaderCell scope="row">1</CTableHeaderCell>
-            <CTableDataCell>Mark</CTableDataCell>
-            <CTableDataCell>Otto</CTableDataCell>
-            <CTableDataCell>@mdo</CTableDataCell>
-          </CTableRow>
-          <CTableRow>
-            <CTableHeaderCell scope="row">2</CTableHeaderCell>
-            <CTableDataCell>Jacob</CTableDataCell>
-            <CTableDataCell>Thornton</CTableDataCell>
-            <CTableDataCell>@fat</CTableDataCell>
-          </CTableRow>
-          <CTableRow>
-            <CTableHeaderCell scope="row">3</CTableHeaderCell>
-            <CTableDataCell colSpan={2}>Larry the Bird</CTableDataCell>
-            <CTableDataCell>@twitter</CTableDataCell>
-          </CTableRow>
+          {users.map((user, index) => (
+            <CTableRow key={index}>
+              <CTableHeaderCell scope="row">{index + 1}</CTableHeaderCell>
+              <CTableDataCell>{user.nombreUsuario}</CTableDataCell>
+              <CTableDataCell>{user.nombre}</CTableDataCell>
+              <CTableDataCell>{user.apellido}</CTableDataCell>
+              <CTableDataCell>{user.correoElectronico}</CTableDataCell>
+            </CTableRow>
+          ))}
         </CTableBody>
       </CTable>{" "}
     </CContainer>
